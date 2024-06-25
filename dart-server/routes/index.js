db073429b683f727d6527d22c4c99f9dc4004f8f
baebyeolha <qufgk0207@naver.com>
2024-06-26 01:03:14 +0900

    feat: dart api

---
+++ dart-server/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/:id', async (req, res, next) => {
+  console.log(req.params.id)
+  let corpId = req.params.id
+  console.log(corpId)
+  try {
+    const corpDetail = await getCorpDetail(corpId, '2023', '11011', 'M210000');
+    res.json(corpDetail);
+  } catch (error) {
+    next(error);
+  }
+});
+
+async function getCorpDetail(id, bsns_year, reprt_code, idx_cl_code){
+  let crtf_key = process.env.CRTF_KEY;
+
+  const resp = await axios.get(`https://opendart.fss.or.kr/api/fnlttSinglIndx.json?crtfc_key=${crtf_key}&corp_code=${id}&bsns_year=${bsns_year}&reprt_code=${reprt_code}&idx_cl_code=${idx_cl_code}`);
+  console.log(resp.data)
+  console.log("fuck api")
+  return resp.data;
+}
+
+
+module.exports = router;
